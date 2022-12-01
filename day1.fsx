65e60a0827d8680a744b777270eb0f74c3f18ca9
Jarkko Palokangas <jarkko.palokangas@solita.fi>
2022-12-01 18:42:52 +0200

    Solve day 1 part 2

--- day1.fsx
+++ day1.fsx
@@ -2,8 +2,19 @@
 
 let input = "inputs/day01.txt" |> System.IO.File.ReadAllText
 
-input.Split("\n\n")
+let calories = 
+    input.Split("\n\n")
     |> Array.map (fun x -> x.Split("\n"))
-    |> Array.map (Array.map int) 
+    |> Array.map (Array.map int)     
+
+// Part one
+calories
     |> Array.map Array.sum
     |> Array.max
+
+// Part two
+calories
+    |> Array.sortByDescending Array.sum
+    |> Array.take 3
+    |> Array.map Array.sum |> Array.sum
+
