09fa9432a606304f97a44a4d0e45ed0d411a7392
Jarkko Palokangas <hpmandito@gmail.com>
2022-12-03 16:12:37 +0200

    Solve day 3 part 2

--- day03.fsx
+++ day03.fsx
@@ -1,20 +1,35 @@
 # time
 
-let duplicates x =
-    match x with
-    | [|a; b|] -> Set.intersect (set a) (set b)
-    | _ -> failwith "Invalid chunks"
+let commons x =
+    x
+    |> Array.map set
+    |> Set.intersectMany
 
 let points (c: char) =
     match c with
     | c when int c <= int 'Z' -> int c - int 'A' + 27
     | _ -> int c - int 'a' + 1
 
-"inputs/day03.txt" 
-    |> System.IO.File.ReadAllLines
+let comparements rucksacks =
+    rucksacks
     |> Array.map Seq.toArray
     |> Array.map (fun x -> Array.splitInto 2 x)
-    |> Array.map (duplicates >> Set.toArray)
+
+let groups rucksacks =
+    rucksacks
+    |> Array.chunkBySize 3
+    |> Array.map (fun x -> Array.map Seq.toArray x)
+    |> Array.map Seq.toArray
+
+let solve part grouping =
+    "inputs/day03.txt" 
+    |> System.IO.File.ReadAllLines
+    |> grouping
+    |> Array.map (commons >> Set.toArray)
     |> Array.collect id
     |> Array.map points
     |> Array.sum
+    |> printfn "Part %s %A" "part"
+
+solve "one" comparements
+solve "two" groups
