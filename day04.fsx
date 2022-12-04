44c24a2d5cbac997ef031267a8511475bc548f07
Jarkko Palokangas <hpmandito@gmail.com>
2022-12-04 18:00:46 +0200

    Solve day 4 part 2
    Refactor day 4

--- day04.fsx
+++ day04.fsx
@@ -3,22 +3,30 @@
 let split (separator: string) (s: string) = s.Split separator
 
 let rangeToSections (first, last) =
-    Array.init (last - first + 1) (fun x -> first + x)
+    set [first..last]
 
 let parseRange range =
     range
     |> split "-"
     |> fun x -> (int x.[0], int x.[1])
 
-let findOverlaps sections =
+let findFullOverlaps (sections: Set<int>[]) =
     sections
-    |> Array.map (Array.map set)
-    |> Array.map (fun a-> (Set.isSubset a.[0] a.[1]) || Set.isSubset a.[1] a.[0])
+    |> (fun a-> (Set.isSubset a.[0] a.[1]) || Set.isSuperset a.[0] a.[1])
 
-"inputs/day04.txt"
-    |> System.IO.File.ReadAllLines
-    |> Array.map (split "," >> Array.map (parseRange >> rangeToSections))
-    |> findOverlaps
+let ranges =
+    "inputs/day04.txt"
+        |> System.IO.File.ReadAllLines
+        |> Array.map (split "," >> Array.map (parseRange >> rangeToSections))
+
+ranges
+    |> Array.map findFullOverlaps
     |> Array.filter id 
     |> Array.length
     |> printfn "Part one: %A"
+
+ranges
+    |> Array.map Set.intersectMany
+    |> Array.filter (Set.isEmpty >> not)
+    |> Array.length
+    |> printfn "Part two: %A"
