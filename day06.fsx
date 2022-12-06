627df422ba3ce3a9b196720b56e766bc14b2b79a
Jarkko Palokangas <hpmandito@gmail.com>
2022-12-06 13:09:14 +0200

    Solve day 6 part 2

--- day06.fsx
+++ day06.fsx
@@ -1,10 +1,21 @@
 # time
 
-"inputs/day06.txt"
+let input =
+    "inputs/day06.txt"
     |> System.IO.File.ReadAllText
+
+let findFirstUniqueIndex length text =
+    text
     |> Seq.toArray
-    |> Array.windowed 4
+    |> Array.windowed length
     |> Array.map set
-    |> Array.findIndex (fun x -> x.Count = 4)
-    |> fun x -> x + 4
+    |> Array.findIndex (fun x -> x.Count = length)
+    |> fun x -> x + length
+
+input
+    |> findFirstUniqueIndex 4
     |> printfn "Part one %A"
+
+input
+    |> findFirstUniqueIndex 14
+    |> printfn "Part two %A"
