adc817b789957606884f8f1a4463102df347a93f
Jarkko Palokangas <jarkko.palokangas@solita.fi>
2022-12-07 08:47:44 +0200

    Clean code

--- day05.fsx
+++ day05.fsx
@@ -30,25 +30,21 @@ let cratesAndMoves =
     |> System.IO.File.ReadAllText
     |> toCratesAndMoves
 
-let rec moveFromTo (stack: char[][]) (amount:int) (fromIdx:int) (toIdx:int) =
-    let takeSize = amount
-    let items = Array.take takeSize stack.[fromIdx]
-    let newFrom = Array.removeManyAt 0 takeSize stack.[fromIdx]
+let rec moveMultiple (amount:int) (fromIdx:int) (toIdx:int) (stack: char[][]) =
+    let items = Array.take amount stack.[fromIdx]
+    let newFrom = Array.removeManyAt 0 amount stack.[fromIdx]
     let newTo = Array.insertManyAt 0 items stack.[toIdx]
     
     stack
     |> Array.updateAt fromIdx newFrom
     |> Array.updateAt toIdx newTo
 
-let rec moveOneAtATime (amount: int) (stack: char[][]) (fromIdx:int) (toIdx:int) =
+let rec moveOneAtATime (amount: int) (fromIdx:int) (toIdx:int) (stack: char[][]) =
     match amount with
     | 0 -> stack
     | _ ->
-        let newStack = moveFromTo stack 1 fromIdx toIdx
-        moveOneAtATime (amount - 1) newStack fromIdx toIdx
-
-let moveMultiple (amount: int) (stack: char[][]) (fromIdx:int) (toIdx:int) =
-    moveFromTo stack amount fromIdx toIdx
+        let newStack = moveMultiple 1 fromIdx toIdx stack
+        moveOneAtATime (amount - 1) fromIdx toIdx newStack
 
 let rec step moveMethod (crates: char[][]) (instructions: int list list)  =
     match instructions with
@@ -56,7 +52,7 @@ let rec step moveMethod (crates: char[][]) (instructions: int list list)  =
         let amount = head.[0]
         let fromIdx = head.[1] - 1
         let toIdx = head.[2] - 1
-        let newCrates = moveMethod amount crates fromIdx toIdx
+        let newCrates = moveMethod amount fromIdx toIdx crates
         step moveMethod newCrates tail
     | _ -> crates
 
